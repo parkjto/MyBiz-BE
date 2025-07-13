c5290c63dedd402980dd434b164a9ce3b6d55b97
parkjto <soomj99@gmail.com>
2025-07-14 01:16:19 +0900

    feat: 프로젝트 구조 및 예시 파일, README 작성

---
+++ controllers/reviewsController.js
@@ -0,0 +1,4 @@
+// 리뷰 비즈니스 로직 예시
+exports.getReviews = (req, res) => {
+  res.json({ message: '고객 감정 분석 기능 (예시)' });
+};
---
+++ models.js
@@ -0,0 +1,10 @@
+// DB 모델 예시 (MongoDB 연동 시 사용)
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const ExampleSchema = new Schema({
+//   name: String,
+//   value: Number
+// });
+
+// module.exports = mongoose.model('Example', ExampleSchema);
